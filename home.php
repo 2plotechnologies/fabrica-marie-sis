a250411c9d26bd36fd081b51252da7019c71bbd6
isaiasgj0002 <isaiasguzman863@gmail.com>
2025-03-07 12:52:58 -0500

    Menu superior

--- home.php
+++ home.php
@@ -27,30 +27,7 @@
 	<!-- pageContent -->
 	<section class="full-width pageContent">
 		<!-- navBar -->
-		<div class="full-width navBar">
-			<div class="full-width navBar-options">
-				<i class="zmdi zmdi-swap btn-menu" id="btn-menu"></i>	
-				<div class="mdl-tooltip" for="btn-menu">Hide / Show MENU</div>
-				<nav class="navBar-options-list">
-					<ul class="list-unstyle">
-						<li class="btn-Notification" id="notifications">
-							<i class="zmdi zmdi-notifications"></i>
-							<div class="mdl-tooltip" for="notifications">Notifications</div>
-						</li>
-						<li class="btn-exit" id="btn-exit">
-							<i class="zmdi zmdi-power"></i>
-							<div class="mdl-tooltip" for="btn-exit">LogOut</div>
-						</li>
-						<li class="text-condensedLight noLink" ><small>User Name</small></li>
-						<li class="noLink">
-							<figure>
-								<img src="assets/img/avatar-male.png" alt="Avatar" class="img-responsive">
-							</figure>
-						</li>
-					</ul>
-				</nav>
-			</div>
-		</div>
+		<?php include('menus/menu_navbar.php'); ?>
 		<section class="full-width text-center" style="padding: 40px 0;">
 			<h3 class="text-center tittles">RESPONSIVE TILES</h3>
 			<!-- Tiles -->
--- menus/menu_lateral.php
+++ menus/menu_lateral.php
@@ -2,7 +2,7 @@
 		<div class="full-width navLateral-bg btn-menu"></div>
 		<div class="full-width navLateral-body">
 			<div class="full-width navLateral-body-logo text-center tittles">
-				<i class="zmdi zmdi-close btn-menu"></i> Inventory 
+				<i class="zmdi zmdi-close btn-menu"></i> Sistema de Administración 
 			</div>
 			<figure class="full-width navLateral-body-tittle-menu">
 				<div>
